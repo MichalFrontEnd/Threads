9e319521d6d94d151db6db3e2bbc87ef4ca100e7
Michal Front <michal.front@outlook.com>
2021-05-25 20:24:53 +0200

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,19 +1,15 @@
 import * as io from "socket.io-client";
 import { chatHistory, displayMsg } from "./actions";
 
-//import actions
-
 export let socket;
 
 export const init = (store) => {
     if (!socket) {
         socket = io.connect();
         socket.on("chatHistory", (data) => {
-            //console.log("data in chatHistory: ", data);
             store.dispatch(chatHistory(data));
         });
         socket.on("displayMsg", (data) => {
-            //console.log("data in displayMsg: ", data);
             store.dispatch(displayMsg(data));
         });
     }
