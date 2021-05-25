9cfba64cddd5c454eaedd70a2776fbb0b75e02d9
Michal Front <michal.front@outlook.com>
2021-05-25 19:27:26 +0200

    Update db.js

--- db.js
+++ db.js
@@ -9,7 +9,6 @@ module.exports.logCreds = (first, last, email, pwd) => {
         "INSERT INTO users (first, last, email, pwd) VALUES ($1, $2, $3, $4) RETURNING *";
 
     let params = [first, last, email, pwd];
-    //console.log("params in logCreds:", params);
     return db.query(q, params);
 };
 
@@ -17,14 +16,12 @@ module.exports.getPwd = function (email) {
     let q = "SELECT first, pwd, id FROM users WHERE email = $1";
 
     let params = [email];
-    //console.log("q, params: ", q, params);
     return db.query(q, params);
 };
 
 module.exports.storeCode = (email, code) => {
     let q = "INSERT INTO pwd_codes (email, code) VALUES ($1, $2)";
     let params = [email, code];
-    //console.log("params in storeCode: ", params);
     return db.query(q, params);
 };
 
@@ -33,14 +30,12 @@ module.exports.checkCode = function (email) {
         "SELECT * FROM pwd_codes WHERE email = $1 AND CURRENT_TIMESTAMP - created_at < INTERVAL '10 minutes' ORDER BY id DESC LIMIT 1";
     //WHERE CURRENT_TIMESTAMP - created_at < INTERVAL '10 minutes';
     let params = [email];
-    //console.log("params in checkCode: ", params);
     return db.query(q, params);
 };
 
 module.exports.updatePassword = (email, pwd) => {
     let q = "UPDATE users SET pwd = $2 WHERE email = $1";
     let params = [email, pwd];
-    //console.log("params in updatePassword: ", params);
     return db.query(q, params);
 };
 
@@ -48,21 +43,18 @@ module.exports.getUserInfo = (id) => {
     let q = "SELECT first, last, email, bio, url FROM users WHERE id = $1";
 
     let params = [id];
-    //console.log("params in getUserInfo: ", params);
     return db.query(q, params);
 };
 
 module.exports.newProfileImage = (id, url) => {
     let q = "UPDATE users SET url =$2 WHERE id=$1 RETURNING url";
     let params = [id, url];
-    //console.log("params: ", params);
     return db.query(q, params);
 };
 
 module.exports.bioUpdate = (id, bio) => {
     let q = "UPDATE users SET bio =$2 WHERE id=$1 RETURNING bio";
     let params = [id, bio];
-    //console.log("params: ", params);
     return db.query(q, params);
 };
 
@@ -74,14 +66,12 @@ module.exports.searchUser = (val) => {
     let q = "SELECT * FROM users WHERE first ILIKE $1 OR last ILIKE $1";
 
     let params = [val + "%"];
-    //console.log("params in searchUser: ", params);
     return db.query(q, params);
 };
 
 module.exports.checkFriendship = (sender_id, rec_id) => {
     let q = `SELECT * FROM friendships WHERE(rec_id = $1 AND sender_id = $2) 
     OR(rec_id = $2 AND sender_id = $1)`;
-
     let params = [sender_id, rec_id];
     return db.query(q, params);
 };
@@ -99,7 +89,6 @@ module.exports.acceptRequest = (sender_id, rec_id) => {
         "UPDATE friendships SET accepted = true WHERE sender_id=$2 and rec_id=$1 RETURNING accepted";
 
     let params = [sender_id, rec_id];
-    //console.log("params in acceptRequest: ", params);
     return db.query(q, params);
 };
 
@@ -108,7 +97,6 @@ module.exports.deleteFriendship = (sender_id, rec_id) => {
         "DELETE FROM friendships WHERE sender_id=$1 AND rec_id=$2 OR rec_id = $1 AND sender_id = $2";
 
     let params = [sender_id, rec_id];
-    //console.log("params in deleteFriendship: ", params);
     return db.query(q, params);
 };
 
@@ -136,14 +124,12 @@ module.exports.storeMessage = (id, msg) => {
     const q =
         "INSERT INTO chat_messages (sender_id, message) VALUES ($1, $2) RETURNING *";
     let params = [id, msg];
-    //console.log("params in storeMessage: ", params);
     return db.query(q, params);
 };
 
 module.exports.displayMessage = () => {
     const q =
         "SELECT message, ts, first, last, url, users.id FROM chat_messages LEFT JOIN users ON users.id=chat_messages.sender_id ORDER BY chat_messages.id DESC LIMIT 1";
-
     return db.query(q);
 };
 
@@ -166,7 +152,6 @@ module.exports.addNewPost = (poster, postee, content) => {
 module.exports.displayPost = (post_id) => {
     const q =
         "SELECT content, ts, first, last, url, users.id, sender_id FROM wall_posts LEFT JOIN users ON users.id=wall_posts.sender_id WHERE wall_posts.id=$1";
-
     let params = [post_id];
     console.log("params: ", params);
     return db.query(q, params);
@@ -176,6 +161,5 @@ module.exports.deletePost = (post_id) => {
     let q = "DELETE FROM wall_posts WHERE id=$1";
 
     let params = [post_id];
-    //console.log("params in deleteFriendship: ", params);
     return db.query(q, params);
 };
